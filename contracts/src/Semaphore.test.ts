d559fdfc5f6711abd0695cff7796865e4f3dba60
Daniel Silberschmidt <daniel.silberschmidt@gmail.com>
2024-08-27 19:24:47 +0200

    esqueleto

---
+++ contracts/src/Semaphore.test.ts
@@ -0,0 +1,7 @@
+// import { semaphore } from './semaphore';
+
+describe('semaphore.js', () => {
+  describe('semaphore()', () => {
+    it.todo('should be correct');
+  });
+});
---
+++ contracts/src/Semaphore.ts
@@ -0,0 +1,3 @@
+export class Semaphore {
+    // Your code here
+}
